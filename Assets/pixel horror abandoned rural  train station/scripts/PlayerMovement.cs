25c1808f1f6f6e213390007a031cb689f4e02ddd
timmonsjames <timmonsj@bxscience.edu>
2025-10-27 12:35:49 -0400

    Start Screen check and fix jump

--- Assets/pixel horror abandoned rural  train station/scripts/PlayerMovement.cs
+++ Assets/pixel horror abandoned rural  train station/scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace littleDog
@@ -11,6 +12,7 @@ namespace littleDog
         public float speed = 12f;
         public float gravity = -9.81f;
         public float JumpHight = 3;
+        public Boolean jumping = false;
          Vector3 V;
         private void Awake()
         {
@@ -24,14 +26,16 @@ namespace littleDog
             if (controller.isGrounded && V.y < 0)
             {
                 V.y = -2f;
+                jumping = false;
             }
             float X = Input.GetAxis("Horizontal");
             float Z = Input.GetAxis("Vertical");
             Vector3 M = transform.right * X + transform.forward * Z;
             controller.Move(M * speed * Time.deltaTime);
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump") && !jumping)
             {
                 V.y = Mathf.Sqrt(JumpHight * -2f * gravity);
+                jumping = true;
             }
             V.y += gravity * Time.deltaTime;
             controller.Move(V * Time.deltaTime);
