1c365a5a6bc0b1020e235e942db56cbd28cf9ff9
HiImTao <taouwu@gmail.com>
2025-10-31 00:09:05 -0400

    music

--- Assets/Scripts/CandyCollectible.cs
+++ Assets/Scripts/CandyCollectible.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class CandyCollectible : MonoBehaviour
 {
     public int scoreValue = 1;
+    public AudioClip collectSound;
+    public float collectVolume = 1f;
+    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
+        if (!other.CompareTag("Player")) return;
+
+       
+        if (ScoreManager.Instance != null)
             ScoreManager.Instance.AddScore(scoreValue);
-            Destroy(gameObject);
-        }
+        else
+            Debug.LogWarning("CandyCollectible: ScoreManager.Instance is null.");
+
+        
+        if (collectSound != null)
+            AudioSource.PlayClipAtPoint(collectSound, transform.position, collectVolume);
+
+        
+
+        Destroy(gameObject);
     }
 }
